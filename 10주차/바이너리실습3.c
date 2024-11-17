77b6ff2ddd415f1d77c21b938dd388e0fa717f4d
zooyeom <ssamtong14@naver.com>
2024-11-17 23:48:21 +0900

    Create 바이너리실습3_1.c

---
+++ 10주차/바이너리실습3.c
@@ -0,0 +1,38 @@
+//구조체를 이용해서 "struct.bin"에 랜덤한 인수 적기
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+typedef struct point {
+	int x;
+	int y;
+}POINT;
+POINT makePoint(void) {
+	POINT p;
+	scanf("%d %d", &p.x, &p.y);
+	return p;
+}
+void makeRandomPoints(POINT p[], int size) {
+	srand(time(NULL));
+	for (int i = 0; i < size;i++) {
+		p[i].x = rand() % 100;
+		p[i].y = rand() % 100;
+	}
+}
+void printPoints(POINT p[], int size) {
+	for (int i = 0;i < size;i++)
+		printf("point[%d] = x : %d, y : %d\n", i, p[i].x, p[i].y);
+}
+void saveFile(POINT p[], int size) {
+	FILE* fp;
+	fp = fopen("struct.bin", "wb");
+	if (fp == NULL) exit(-1);
+	fwrite(p, sizeof(POINT), size, fp);
+	fclose(fp);
+}
+int main() {
+	POINT p[10];
+	makeRandomPoints(p, 10);
+	printPoints(p, 10);
+	saveFile(p,10);
+}

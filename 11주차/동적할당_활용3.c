97ce85ecec58dc8799f9a491e6084d9b9cd0dddd
zooyeom <ssamtong14@naver.com>
2024-11-18 14:41:26 +0900

    Update 동적할당_활용3.c

--- 11주차/동적할당_활용3.c
+++ 11주차/동적할당_활용3.c
@@ -7,7 +7,7 @@ typedef struct point {
 }POINT;
 int inputMenu(void) {
 	int input;
-	printf("메뉴입력(1.구조체 만들기, 2.구조체 출력하기, -1.종료) :");
+	printf("메뉴입력(1.만들기, 2.출력하기, 3.클리어, 4.파일저장 , 5.파일로드, -1.종료) :");
 	scanf("%d", &input);
 	return input;
 }
@@ -18,15 +18,42 @@ POINT* makePoint(void) {
 	p->y = rand() % 100;
 	return p;
 }
-
 void printPoint(POINT* p[], int size) {
 	for (int i = 0;i < size;i++) {
 		printf("x:%d, y=%d\n", p[i]->x, p[i]->y);
 	}
 }
-
 void freePoints(POINT* p[], int size) {
 	for (int i = 0;i < size;i++) free(p[i]);
+}
+int saveFile(POINT* p[], int count) {
+	FILE* fp;
+	fp = fopen("points.bin", "ab");
+	if (fp == NULL) return 0;
+	for (int i = 0;i < count;i++) {
+		fwrite(p[i], sizeof(POINT), 1, fp);
+	fclose(fp);
+	return 1;
+	}
+}
+int loadFile(POINT* p[], int max) {
+	FILE* fp;
+	int fSize, fCount;
+	fp = fopen("points.bin", "rb");
+	if (fp == NULL) return 0;
+
+	fseek(fp, 0, SEEK_END);
+	fSize = ftell(fp);
+	fCount = fSize / sizeof(POINT);
+	rewind(fp);
+
+	for (int i = 0;i < fCount;i++) {
+		if (i == max) break;
+		p[i] = (POINT*)malloc(sizeof(POINT));
+		fread(p[i], sizeof(POINT), 1, fp);
+	}
+	fclose(fp);
+	return fCount;
 }
 int main() {
 	int menu;
@@ -34,17 +61,28 @@ int main() {
 	POINT* points[100];
 	do {
 		menu = inputMenu();
-		if (menu == 1) {
+		switch (menu) {
+		case 1:
 			points[count] = makePoint();
 			count++;
-			printf("잘 만들었습니다.\n");
-		}
-		else if (menu == 2) {
+			break;
+		case 2:
 			printPoint(points, count);
-		}
-		else if (menu == -1) {
+			break;
+		case 3:
+			system("cls");
+			break;
+		case 4:
+			if (!saveFile(points, count)) printf("file open error...\n");
+			else printf("file writing success...\n");
+			break;
+		case 5:
+			count = loadFile(points, 100);
+			break;
+		case -1:
 			freePoints(points, count);
 			printf("프로그램을 종료합니다.\n");
+			break;
 		}
 	} while (menu > 0);
 }
